1c67fd1f10612e37c02ccb1c5904c5f62d8e67b1
Yathavan <yathavancom@gmail.com>
2025-12-21 04:16:39 +0100

    login form created

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <div class="flex w-full h-full justify-center items-center">
@@ -30,10 +34,27 @@
       <div class="w-1/2 flex justify-center items-center">
         <div class="w-full h-auto max-w-[432px] flex flex-col justify-start items-start">
           <img src="/img/login-Icon.svg" class="mb-6" alt="login icon" />
-          <h2 class="title mb-1">Login</h2>
+          <h2 class="title text-foreground mb-1">Login</h2>
           <p class="body text-secondary mb-6">
             Don’t have an account? <a class="text-primary" href="#">Sign up</a>
           </p>
+          <form
+            class="w-full [&_label]:after:content-['*'] [&_label]:after:text-red-500 flex flex-col gap-6"
+          >
+            <div class="grid w-full items-center gap-1.5">
+              <Label for="email">Email</Label>
+              <Input id="email" type="email" placeholder="Email" />
+            </div>
+            <div class="grid w-full items-center gap-1.5">
+              <div class="flex justify-between items-center">
+                <Label for="password">Password</Label>
+                <a href="#" class="small-body font-semibold text-primary"> Forgot password? </a>
+              </div>
+              <Input id="password" type="password" placeholder="" />
+            </div>
+
+            <Button class="w-full">Login</Button>
+          </form>
         </div>
       </div>
     </div>
